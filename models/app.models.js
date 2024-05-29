0926bb2d581358c9d75e82d9009431f6ce257234
sidendev <96108993+sidendev@users.noreply.github.com>
2024-05-29 17:22:29 +0100

    Add GET: api/articles for task 5, including tests, updated endpoints.json

--- models/app.models.js
+++ models/app.models.js
@@ -30,3 +30,25 @@ exports.selectArticleById = (article_id) => {
       return rows[0];
     });
 };
+
+exports.selectArticles = () => {
+  return db
+    .query(
+      `SELECT
+        articles.article_id,
+        articles.title,
+        articles.topic,
+        articles.author,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+      COUNT(comments.comment_id)::INT AS comment_count 
+      FROM articles 
+      LEFT JOIN comments ON articles.article_id = comments.article_id 
+      GROUP BY articles.article_id 
+      ORDER BY articles.created_at DESC;`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
