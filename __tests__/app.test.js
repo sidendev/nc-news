35d040c45d89d8ac52320fc4b71d52f9bd48b64f
sidendev <96108993+sidendev@users.noreply.github.com>
2024-05-29 13:35:58 +0100

    Add test to GET/api to check for comparison of api file to original

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 const db = require('../db/connection');
 const seed = require('../db/seeds/seed');
 const data = require('../db/data/test-data');
+const fs = require('fs');
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
@@ -32,6 +33,18 @@ describe('GET/api', () => {
         for (let endpoint in endpointDetails) {
           expect(typeof endpointDetails[endpoint].description).toBe('string');
         }
+      });
+  });
+  test('GET:200 compares original json file to be equal to the data sent back from api', () => {
+    const originalEndpointDetails = fs.readFileSync(
+      './endpoints.json',
+      'utf-8'
+    );
+    return request(app)
+      .get('/api')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.details).toEqual(JSON.parse(originalEndpointDetails));
       });
   });
 });
