c431dab6f2c63a9eb669a72f4ef701c450b7d665
sidendev <96108993+sidendev@users.noreply.github.com>
2024-07-22 22:08:09 +0100

    Add data for articles, topics and users for testing

--- db/data/development-data/topics.js
+++ db/data/development-data/topics.js
@@ -1,5 +1,11 @@
 module.exports = [
   { description: 'Code is love, code is life', slug: 'coding' },
-  { description: 'FOOTIE!', slug: 'football' },
-  { description: 'Hey good looking, what you got cooking?', slug: 'cooking' }
+  { description: 'FOOTIE, FOOTIE, FOOTIE!', slug: 'football' },
+  { description: 'Hey good looking, what you got cooking?', slug: 'cooking' },
+  { description: "Let's Laugh", slug: 'funny' },
+  { description: 'They do the funniest things!', slug: 'animals' },
+  { description: 'You daily dose of positivity', slug: 'uplifting' },
+  { description: 'Good news for the soul', slug: 'health' },
+  { description: 'We love you Earth', slug: 'environment' },
+  { description: 'People are amazing', slug: 'heros' },
 ];
